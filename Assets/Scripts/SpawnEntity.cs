090f96d5ac9e894b9799c30d2db2e5da5133c179
Guilherme Rosada <62749179+Rosada-eng@users.noreply.github.com>
2023-04-29 15:31:43 -0300

    fix spawn randomize

--- Assets/Scripts/SpawnEntity.cs
+++ Assets/Scripts/SpawnEntity.cs
@@ -19,14 +19,14 @@ public class SpawnEntity : MonoBehaviour
             float random = Random.Range(0f, 1f);
             if (random < spawnProbability)
             {
-                child.gameObject.SetActive(false);
-                childrenStatus.Add(false);
+                child.gameObject.SetActive(true);
+                childrenStatus.Add(true);
                 Debug.Log("child " + i + " ACTIVE - position: " + child.position.x + ", " + child.position.y + ", " + child.position.z);
             }
             else
             {
-                child.gameObject.SetActive(true);
-                childrenStatus.Add(true);
+                child.gameObject.SetActive(false);
+                childrenStatus.Add(false);
             }
 
 
