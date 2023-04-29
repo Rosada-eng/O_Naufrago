d1ec2aaef4ca771032ffc3e64597973cc671ea54
Guilherme Rosada <62749179+Rosada-eng@users.noreply.github.com>
2023-04-29 15:57:05 -0300

    add all spawns

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -7,27 +7,15 @@ public class Boat : MonoBehaviour
     // Start is called before the first frame update
     public Sprite[] sprites;
 
-    private Vector3[] positions = {
-        new Vector3(-11f, 14f, 0f),
-        new Vector3(60f, 40f, 0f)
-    };
+    // TODO: Change sprite when is able to travel (fixed)
 
-    private Vector3[] rotations = {
-        new Vector3(0f, 0f, 0f),
-        new Vector3(0f, 0f, 45f)
-    };
-
-    void Awake()
+    void Start()
     {
         // select a random sprite
         int index = Random.Range(0, sprites.Length);
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = sprites[index];
 
-        // select a random spawn
-        index = Random.Range(0, positions.Length);
-        transform.position = positions[index];
-        transform.eulerAngles = rotations[index];
 
     }
 
