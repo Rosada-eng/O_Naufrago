df014a8e79be3570cabc6e48c5d53a25d161b375
Guilherme Rosada <62749179+Rosada-eng@users.noreply.github.com>
2023-05-01 16:29:10 -0300

    add tag to collectable

--- Assets/Scripts/CollectableSprite.cs
+++ Assets/Scripts/CollectableSprite.cs
@@ -6,9 +6,22 @@ public class CollectableSprite : MonoBehaviour
 {
     public Sprite[] sprites;
 
-    public void Awake() {
+    private Dictionary<int, string> spriteTag = new Dictionary<int, string>(){
+        {0, "Wood"},
+        {1, "Wood"},
+        {2, "Fabric"},
+        {3, "Fabric"},
+        {4, "Fabric"},
+        {5, "Rope"},
+        {6, "Rope"},
+        {7, "Rope"},
+    };
+    public void Awake()
+    {
         int index = Random.Range(0, sprites.Length);
         GetComponent<SpriteRenderer>().sprite = sprites[index];
+
+        transform.parent.tag = spriteTag[index];
     }
 
 }
