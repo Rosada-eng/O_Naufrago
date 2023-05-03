d486cff92315f578cc7623c10260f2234a96af54
Antonioarf <antonio.awad.roxo@gmail.com>
2023-05-02 23:40:58 -0300

    tete

---
+++ Assets/Scripts/cut2.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class cut2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float delay = 10.0f;
+
+    public AudioClip loopSound;
+    public AudioClip onceSound;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = loopSound;
+        audioSource.loop = true;
+        audioSource.Play();
+         Invoke("DelayedFunction", delay);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    void DelayedFunction()
+    {
+        audioSource.Stop();
+        audioSource.clip = onceSound;
+        audioSource.loop = false;
+        audioSource.Play();
+        SceneManager.LoadScene(5);
+    }
+}
--- Assets/Scripts/start.cs
+++ Assets/Scripts/start.cs
@@ -6,13 +6,26 @@ using UnityEngine.SceneManagement;
 public class start : MonoBehaviour
 {   
     public GameObject SettingsMenuUI;
-    public GameObject pauseMenuUI;
+    public GameObject StartMenuUI;
+    public GameObject TextUI;
+    public float delay = 30.0f;
+
+    public AudioClip loopSound;
+    public AudioClip onceSound;
+    public AudioClip thirdSound;
+    public AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
+        audioSource.clip = loopSound;
+        audioSource.loop = true;
+        audioSource.Play();
         SettingsMenuUI.SetActive(false);
-        pauseMenuUI.SetActive(true);    
-        
+        StartMenuUI.SetActive(false);    
+        TextUI.SetActive(true);
+        Invoke("DelayedFunction", delay);
+
     }
 
     // Update is called once per frame
@@ -20,16 +33,35 @@ public class start : MonoBehaviour
     {
         
     }
+
+    
+    void DelayedFunction()
+    {
+        TextUI.SetActive(false);
+        StartMenuUI.SetActive(true);
+        audioSource.Stop();
+        audioSource.clip = onceSound;
+        audioSource.loop = false;
+        audioSource.Play();
+        Invoke("PlayThirdSound", 2.0f);
+    }
+
+    private void PlayThirdSound()
+    {
+        audioSource.clip = thirdSound;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
     public void press_settings(){
         Debug.Log("settings");
         SettingsMenuUI.SetActive(true);
-        pauseMenuUI.SetActive(false);
+        StartMenuUI.SetActive(false);
         }
     
     public void press_back(){
         Debug.Log("back");
         SettingsMenuUI.SetActive(false);
-        pauseMenuUI.SetActive(true);
+        StartMenuUI.SetActive(true);
     }
 
     public void press_quit(){
@@ -42,3 +74,11 @@ public class start : MonoBehaviour
         SceneManager.LoadScene(1);
         }
 }
+
+// Bem-vindo ao Arquipélago Sombrio, sobrevivente! 
+// Você está prestes a embarcar em uma jornada emocionante e perigosa. 
+// Rumores dizem que coisas estranhas acontecem nesta ilha aparentemente desabitada,
+// Mas seu avião passou por uma pane e você foi forçado a abandoná-lo, acionando seu paraquedas.
+// O destino te colocou aqui, agora soó resta fugir, ou morrer tentando. 
+// Com poucos recursos e em um lugar desconhecido, você precisará usar sua coragem e instintos para explorar a ilha e encontrar uma maneira de sobreviver
+// E a única saída parece ser o mar. Boa sorte!
